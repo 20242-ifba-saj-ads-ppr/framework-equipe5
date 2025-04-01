c62c813c149ebbc743021c620246bb76e3c5aa83
Mr Jabes <jabessilva31@gmail.com>
2025-04-01 18:10:47 -0300

    Atualizações:
    * Adiciona diagramas UML
    
    * Enums para estrutura do jogo de tabuleiro com padrão Multiton

---
+++ src/main/java/br/com/frameworkPpr/xadrez/multiton/movimento/Movimento.java
@@ -0,0 +1,39 @@
+package main.java.br.com.frameworkPpr.xadrez.multiton.movimento;
+
+/**
+ * Enum representa as possiveis movimentações da peça no jogo de tabuleiro.
+ * Cada peça pode se movimentar varias vezes considerando o plano cartesiano do tabuleiro.
+ * Padrão de projeto: Enum Mutiton.
+ */
+
+public enum Movimento {
+    PRAFRENTE(0, 1),
+    PRATRAZ(0,-1),
+    ESQUERDA(-1, 0),
+    DIREITA(+1, 0);
+    
+    private int x;
+    private int y;
+
+    Movimento (int x, int y){
+       setX(x);
+       setY(y);
+    }
+
+    public void setY( int y) {
+        this.y = y;
+    }
+    public int getY() {
+        return y;
+    }
+    public int getX (){
+        return x;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+    public void executarMovimento (){
+
+    }
+}
