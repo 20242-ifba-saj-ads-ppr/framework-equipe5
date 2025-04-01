a391202a38ad92edc0458e3916e27accd74801d7
Mr Jabes <jabessilva31@gmail.com>
2025-04-01 18:11:16 -0300

    Atualizações:
    * Adiciona implementação do padrão Singleton para o jogo de tabuleiro e diagramas UML explicativos

---
+++ src/main/java/br/com/frameworkPpr/xadrez/singleton/FrameworkJogoDeTabuleiro.Java
@@ -0,0 +1,41 @@
+package frameworkJogoDeTabuleiro.model.singleton;
+
+/**
+ * A classe FrameworkJogoDetabuleiro é um exemplo da aplicação 
+ * do padrão de projeto Singleton. Ele é aplicado para estanciar o jogo de tabuleiro.
+ * Isso garante que haja apenas uma instância do jogo de tabuleiro em toda a aplicação.
+ */
+public class FrameworkJogoDeTabuleiro {
+
+
+    private String nomeJogo;
+
+    private static FrameworkJogoDeTabuleiro estanciaJogo;
+
+
+    private FrameworkJogoDeTabuleiro(String nomeJogo) {
+        setNomeJogo(nomeJogo);
+    }
+
+    public static FrameworkJogoDeTabuleiro getIstance(String nomeJogo) {
+        if(getEstanciaJogo() == null){
+            setEstanciaJogo(new FrameworkJogoDeTabuleiro(nomeJogo));
+        }
+        return getEstanciaJogo();
+    }
+
+    public String getNomeJogo() {
+        return nomeJogo;
+    }
+
+    private void setNomeJogo(String nomeJogo) {
+        this.nomeJogo = nomeJogo;
+    }
+    private static FrameworkJogoDeTabuleiro getEstanciaJogo() {
+        return estanciaJogo;
+    }
+
+    private static void setEstanciaJogo(FrameworkJogoDeTabuleiro estanciaJogo) {
+        FrameworkJogoDeTabuleiro.estanciaJogo = estanciaJogo;
+    }
+}
