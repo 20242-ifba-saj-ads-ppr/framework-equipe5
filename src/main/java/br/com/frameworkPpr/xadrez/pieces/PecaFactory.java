fe19d51c0ca76efc6581c65da49d70a6835e7122
Mr Jabes <jabessilva31@gmail.com>
2025-04-03 00:14:22 -0300

    refactor: altera método criarPecaGenerica para usar Time em vez de cor

--- src/main/java/br/com/frameworkPpr/xadrez/pieces/PecaFactory.java
+++ src/main/java/br/com/frameworkPpr/xadrez/pieces/PecaFactory.java
@@ -3,12 +3,13 @@ package main.java.br.com.frameworkPpr.xadrez.pieces;
 import java.util.Map;
 
 import main.java.br.com.frameworkPpr.xadrez.movement.MovimentoStrategy;
+import main.java.br.com.frameworkPpr.xadrez.multiton.time.Time;
 
 public abstract class PecaFactory {
     
     public abstract Peca criarPeca(String tipo, MovimentoStrategy movimentoStrategy);
 
-    public Peca criarPecaGenerica(String nome, String cor, MovimentoStrategy movimentoStrategy, Map<String, Object> caracteristicas) {
-        return new Peca(nome, cor, movimentoStrategy, caracteristicas) {};
+    public Peca criarPecaGenerica(String nome, Time time, MovimentoStrategy movimentoStrategy, Map<String, Object> caracteristicas) {
+        return new Peca(nome, time, movimentoStrategy, caracteristicas) {};
     }
 }
