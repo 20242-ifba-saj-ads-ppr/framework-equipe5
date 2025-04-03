3b3d4f8793a0a84bf3a4c13a1fa67033bb60364c
Mr Jabes <jabessilva31@gmail.com>
2025-04-02 23:31:25 -0300

    Add: inclusão do Mutiton de time a lógica do tabuleiro

--- src/main/java/br/com/frameworkPpr/xadrez/board/Tabuleiro.java
+++ src/main/java/br/com/frameworkPpr/xadrez/board/Tabuleiro.java
@@ -1,10 +1,8 @@
 package main.java.br.com.frameworkPpr.xadrez.board;
-
-import java.sql.Time;
 import java.util.HashMap;
 import java.util.Map;
-
 import main.java.br.com.frameworkPpr.GerenciadorVitoriaDerrota.VitoriaDerrotaObserver;
+import main.java.br.com.frameworkPpr.xadrez.multiton.time.Time;
 import main.java.br.com.frameworkPpr.xadrez.pieces.Peca;
 
 public class Tabuleiro {
@@ -16,25 +14,20 @@ public class Tabuleiro {
     public Tabuleiro(int linhas, int colunas) {
         this.linhas = linhas;
         this.colunas = colunas;
-        casas = new HashMap<>();
-        this.pecasPorTime = new HashMap<>();
-        pecasPorTime.put(Time.valueOf("Branco"), 0);
-        pecasPorTime.put(Time.valueOf("Preto"), 0);
-
+        setCasas(new HashMap<>());
+        setPecasPorTime(new HashMap<>());
+        getPecasPorTime().put(Time.BRANCO, 0);
+        getPecasPorTime().put(Time.PRETO, 0);
         for (int linha = 0; linha < linhas; linha++) {
             for (int coluna = 0; coluna < colunas; coluna++) {
                 Posicao posicao = new Posicao(linha, coluna);
-                casas.put(posicao, new Casa(posicao));
+                getCasas().put(posicao, new Casa(posicao));
             }
         }
     }
 
-    public Casa getCasa(Posicao posicao) {
-        return casas.get(posicao);
-    }
-
-    public void colocarPeca(Peca peca, Posicao posicao) {
-        Casa casa = casas.get(posicao);
+    public void colocarPeca(Peca peca, Posicao posicao, Time time) {
+        Casa casa = getCasas().get(posicao);
         if (casa == null) {
             throw new IllegalArgumentException("Posição inválida: " + posicao);
         }
@@ -42,14 +35,12 @@ public class Tabuleiro {
             throw new IllegalArgumentException("A posição já está ocupada: " + posicao);
         }
         casa.setPeca(peca);
-
-        Time time = peca.getTime();
-        pecasPorTime.put(time, pecasPorTime.getOrDefault(time, 0) + 1); // Incrementa o número de peças do time
+        getPecasPorTime().put(time, getPecasPorTime().getOrDefault(time, 0) + 1); // Incrementa o número de peças do time
     }
 
     public void moverPeca(Posicao origem, Posicao destino) {
-        Casa casaOrigem = casas.get(origem);
-        Casa casaDestino = casas.get(destino);
+        Casa casaOrigem = getCasas().get(origem);
+        Casa casaDestino = getCasas().get(destino);
 
         if (casaOrigem == null || casaDestino == null) {
             throw new IllegalArgumentException("Posição de origem ou destino inválida.");
@@ -71,15 +62,14 @@ public class Tabuleiro {
         return posicaoValida(posicao.getLinha(), posicao.getColuna());
     }
 
-    public void RemoverPeca(Posicao posicao)
+    public void RemoverPeca(Posicao posicao, Time time)
     {
-        Casa casa = casas.get(posicao);
+        Casa casa = getCasas().get(posicao);
 
         Peca peca = casa.getPeca();
         casa.setPeca(null);
 
-        Time time = peca.getTime();
-        pecasPorTime.put(time, pecasPorTime.getOrDefault(time, 0) - 1); // Decrementa o número de peças do time
+        getPecasPorTime().put(time, pecasPorTime.getOrDefault(time, 0) - 1); // Decrementa o número de peças do time
     }
 
     public Map<Time, Integer> getPecasPorTime() {
@@ -92,5 +82,20 @@ public class Tabuleiro {
 
     public int getColunas() {
         return this.colunas;
+    }
+
+    public Casa getCasa(Posicao posicao) {
+        return getCasas().get(posicao);
+    }
+
+    public Map<Posicao, Casa> getCasas() {
+        return casas;
+    }
+
+    public void setPecasPorTime(Map<Time, Integer> pecasPorTime) {
+        this.pecasPorTime = pecasPorTime;
+    }
+    public void setCasas(Map<Posicao, Casa> casas) {
+        this.casas = casas;
     }
 }
