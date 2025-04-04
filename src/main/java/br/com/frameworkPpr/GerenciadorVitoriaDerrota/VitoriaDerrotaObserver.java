4b1f8dad10a904ac8b06d3379706a361a97c5279
Mr Jabes <jabessilva31@gmail.com>
2025-04-04 17:04:43 -0300

    fix: corrige construtor da classe VitoriaDerrotaObserver e ajusta lógica de verificação de vencedor

--- src/main/java/br/com/frameworkPpr/GerenciadorVitoriaDerrota/VitoriaDerrotaObserver.java
+++ src/main/java/br/com/frameworkPpr/GerenciadorVitoriaDerrota/VitoriaDerrotaObserver.java
@@ -10,14 +10,17 @@ import main.java.br.com.frameworkPpr.xadrez.multiton.time.Time;
 public class VitoriaDerrotaObserver implements CondicaoDeVitoria{
     private Tabuleiro tabuleiro;
 
-    public void VitoriaDerrotaObserver(Tabuleiro tabuleiro) {
+    public VitoriaDerrotaObserver(Tabuleiro tabuleiro) {
         setTabuleiro(tabuleiro);
     }
 
     @Override
-    public Time verificarVencedor()
-    {
-        return getTabuleiro().getPecasPorTime().get(Time.BRANCO) != null? Time.BRANCO : Time.PRETO;
+    public Time verificarVencedor(){
+        return getTabuleiro().getPecasPorTime().get(Time.BRANCO) == 0? 
+                Time.PRETO 
+                : getTabuleiro().getPecasPorTime().get(Time.PRETO) == 0? 
+                Time.BRANCO 
+                : null;
     }
 
     public Tabuleiro getTabuleiro() {
