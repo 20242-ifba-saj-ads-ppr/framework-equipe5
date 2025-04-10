e4260651d194d6d0765d094ba2413ccfb93abb1e
Mr Jabes <jabessilva31@gmail.com>
2025-04-10 13:49:48 -0300

    fix: corrige importação da classe Tabuleiro para o pacote singletonEProxySecurity

--- src/main/java/br/com/frameworkPpr/xadrez/movement/MovimentoStrategy.java
+++ src/main/java/br/com/frameworkPpr/xadrez/movement/MovimentoStrategy.java
@@ -1,7 +1,8 @@
 package main.java.br.com.frameworkPpr.xadrez.movement;
 
 import main.java.br.com.frameworkPpr.xadrez.board.Posicao;
-import main.java.br.com.frameworkPpr.xadrez.board.Tabuleiro;
+import main.java.br.com.frameworkPpr.xadrez.board.tabuleiro.singletonEProxySecurity.Tabuleiro;
+
 import java.util.List;
 
 public interface MovimentoStrategy {
