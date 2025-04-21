8ab81850c8c0670e964a55447a311538621033de
LuisPassosRamos <20222tadssaj0015@ifba.edu.br>
2025-04-21 17:10:12 -0300

    feat: apply singleton on gamesession

--- src/main/java/br/com/frameworkPpr/boardgame/padroes/criacionais/singleton/GameSession.java
+++ src/main/java/br/com/frameworkPpr/boardgame/padroes/criacionais/singleton/GameSession.java
@@ -7,11 +7,26 @@ public class GameSession {
     private Jogador jogador1;
     private Jogador jogador2;
     private Tabuleiro tabuleiro;
+    private static GameSession instance;
 
-    public GameSession(Jogador jogador1, Jogador jogador2, Tabuleiro tabuleiro) {
+    private GameSession(Jogador jogador1, Jogador jogador2, Tabuleiro tabuleiro) {
         this.jogador1 = jogador1;
         this.jogador2 = jogador2;
         this.tabuleiro = tabuleiro;
+    }
+
+    public static synchronized GameSession getInstance(Jogador jogador1, Jogador jogador2, Tabuleiro tabuleiro) {
+        if (instance == null) {
+            instance = new GameSession(jogador1, jogador2, tabuleiro);
+        }
+        return instance;
+    }
+
+    public static synchronized GameSession getInstance() {
+        if (instance == null) {
+            throw new IllegalStateException("GameSession ainda não foi inicializada.");
+        }
+        return instance;
     }
 
     public Jogador getJogador1() {
@@ -25,9 +40,14 @@ public class GameSession {
     public Tabuleiro getTabuleiro() {
         return tabuleiro;
     }
+
     public void configurarTimes(String... nomesTimes) {
         for (String nomeTime : nomesTimes) {
             tabuleiro.registrarTime(nomeTime);
         }
+    }
+
+    public static synchronized void reset() {
+        instance = null;
     }
 }
