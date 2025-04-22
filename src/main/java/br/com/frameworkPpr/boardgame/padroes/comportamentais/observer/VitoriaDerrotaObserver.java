3fa59ecd733df06f697ad3e9a80ee8d6695e3412
Mr Jabes <jabessilva31@gmail.com>
2025-04-22 15:58:10 -0300

    refactor: reestruturar método verificarVencedor para melhorar a lógica de verificação de vencedor

--- src/main/java/br/com/frameworkPpr/boardgame/padroes/comportamentais/observer/VitoriaDerrotaObserver.java
+++ src/main/java/br/com/frameworkPpr/boardgame/padroes/comportamentais/observer/VitoriaDerrotaObserver.java
@@ -1,5 +1,7 @@
 package main.java.br.com.frameworkPpr.boardgame.padroes.comportamentais.observer;
 
+import java.util.Objects;
+
 import main.java.br.com.frameworkPpr.boardgame.game.Tabuleiro;
 import main.java.br.com.frameworkPpr.boardgame.padroes.criacionais.multiton.Time;
 
@@ -11,12 +13,20 @@ public class VitoriaDerrotaObserver implements Observer, CondicaoDeVitoria{
     }
 
     @Override
-    public Time verificarVencedor(){
-        return getTabuleiro().getPecasPorTime().get(Time.getInstance("Branco")) == 0? 
-                Time.getInstance("Preto") 
-                : getTabuleiro().getPecasPorTime().get(Time.getInstance("Preto")) == 0? 
-                Time.getInstance("Branco") 
-                : null;
+    public Time verificarVencedor() {
+        for (String time : Time.getChaves()) {// Para cada Chave de time registrado
+            Time timeAtual = Time.getInstance(time);// Pego o time
+            if (timeAtual.getQuantidadePecasDoTime() == 0) {// Verifico se a Qtd é igual a 0
+                // Na primeira vez que ele identificar essa condição
+                // Retorna o outro time como vencedor
+                for (String outroTime : Time.getChaves()) {// Outro for para pegaro outro time
+                    if (!Objects.equals(outroTime,time)) {// em que se o outro time não for iqual ao time
+                        return Time.getInstance(outroTime);
+                    }
+                }
+            }
+        }
+        return null; // Nenhum vencedor ainda
     }
 
     public void notificarVencedor(Time vencedor)
